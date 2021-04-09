ea245905f20279ef21be40bf545dd3e23da8fdcc
SushmaNalla <77800401+SushmaNalla@users.noreply.github.com>
2021-04-09 12:29:07 -0500

    update

--- controllers/book.js
+++ controllers/book.js
@@ -12,9 +12,17 @@ exports.book_list = async function (req, res) {
 
 };
 // for a specific Book.
-exports.book_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+exports.book_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await Book.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
+
 // Handle Book create on POST.
 exports.book_create_post = async function (req, res) {
     console.log(req.body)
@@ -40,9 +48,24 @@ exports.book_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Book delete DELETE ' + req.params.id);
 };
 // Handle Book update form on PUT.
-exports.book_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: Book update PUT' + req.params.id);
+exports.book_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Book.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.name) toUpdate.name = req.body.name;
+        if(req.body.price) toUpdate.price = req.body.price;
+        if(req.body.author) toUpdate.author = req.body.author;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
+
+
 // VIEWS
 // Handle a show all view
 exports.book_view_all_Page = async function (req, res) {
